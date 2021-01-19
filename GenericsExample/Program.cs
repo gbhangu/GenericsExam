8f6701e170ae708289ff91db71db671293878ce1
Gurpreet <bhangu_gurpreet@yahoo.com>
2021-01-18 20:00:51 -0500

    first

---
+++ GenericsExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            FinalTest<int> obj = new FinalTest<int>();
+            obj.Add(10);
+        }
+    }
+    public class FinalTest<T> where T : struct
+    {
+         public string value { get; set; }
+        public void Add(T input)
+        {
+            this.value = input;
+        }
+
+    }
+}
